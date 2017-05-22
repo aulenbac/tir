0002f15d9268fd1253c54c4753fd28d43fc829ea
Sky <skybristol@gmail.com>
2017-05-22 13:52:01 -0600

    New ITIS and NatureServe processors

--- TESS.py
+++ TESS.py
@@ -1,20 +1,22 @@
 
 # coding: utf-8
 
-# In[14]:
+# This notebook used ITIS TSNs discovered and cached in the Taxonomic Information Registry to look for information from the USFWS Threatened and Endangered Species System web service. It cached either a negative result or a set of key/value pairs from the TESS service of interest in characterizing species in the TIR.
+
+# In[1]:
 
 import requests,configparser,re
 from IPython.display import display
 
 
-# In[28]:
+# In[2]:
 
 # Get API keys and any other config details from a file that is external to the code.
 config = configparser.RawConfigParser()
 config.read_file(open(r'../config/stuff.py'))
 
 
-# In[16]:
+# In[3]:
 
 # Build base URL with API key using input from the external config.
 def getBaseURL():
@@ -23,7 +25,7 @@ def getBaseURL():
     return apiBaseURL
 
 
-# In[35]:
+# In[4]:
 
 # Basic function to insert subject ID, property, and value into tircache
 def cacheToTIR(gid,infotype,pairs):
@@ -31,14 +33,20 @@ def cacheToTIR(gid,infotype,pairs):
     return requests.get(getBaseURL()+"&q="+updateQ).json()
 
 
-# In[30]:
+# ### Query TESS by TSN
+# There are several potential query points into TESS, but for now, we are just using the TSN-based search.
+
+# In[5]:
 
 def queryTESS(tsn):
     tessSpeciesQueryByTSNBaseURL = "https://ecos.fws.gov/ecp0/TessQuery?request=query&xquery=/SPECIES_DETAIL[TSN="
     return requests.get(tessSpeciesQueryByTSNBaseURL+tsn+"]").text
 
 
-# In[24]:
+# ### Package up the TESS data
+# This function works through the XML result from the TESS service. It will always return the data/time that the packaging step ran, the TSN that was used, and a result code. The result can be "none", "error" or "success". If successful, information is packaged from specific TESS attributes of interest into key/value pairs for later use.
+
+# In[6]:
 
 def packageTESSPairs(tsn,tessData):
     import datetime
@@ -72,21 +80,27 @@ def packageTESSPairs(tsn,tessData):
     return tessPairs
 
 
-# In[33]:
+# ### Get TSNs for processing
+# Right now, we retrieve everything from the TIR table that has an ITIS TSN and does not currently have any TESS data. In future, we should come up with some other way of triggering this processing service and come up with a plan for periodic checks and updates.
 
-tsns = requests.get(getBaseURL()+"&q=SELECT gid,itis->'discoveredTSN' AS discoveredtsn,itis->'acceptedTSN' AS acceptedtsn FROM tir.tir2 WHERE tess IS NULL AND itis->'itisMatchMethod' NOT LIKE 'NotMatched%'").json()
+# In[7]:
+
+tsns = requests.get(getBaseURL()+"&q=SELECT gid,itis->'tsn' AS tsn,itis->'acceptedTSN' AS acceptedtsn FROM tir.tir2 WHERE tess IS NULL AND itis->'itisMatchMethod' NOT LIKE 'NotMatched%'").json()
 
 
-# In[ ]:
+# ### Process the records to query and return information from TESS
+# In this current process, I first check the discovered TSN from the ITIS cache. If that one does not return any results, I check the accepted TSN from that record (if it exists).
+
+# In[8]:
 
 for feature in tsns["features"]:
     thisRecord = {}
     thisRecord["gid"] = feature["properties"]["gid"]
-    thisRecord["discoveredTSN"] = feature["properties"]["discoveredtsn"]
+    thisRecord["tsn"] = feature["properties"]["tsn"]
     thisRecord["acceptedTSN"] = feature["properties"]["acceptedtsn"]
     
     # Query based on discovered TSN and package data
-    thisRecord["tessPairs"] = packageTESSPairs(thisRecord["discoveredTSN"],queryTESS(thisRecord["discoveredTSN"]))
+    thisRecord["tessPairs"] = packageTESSPairs(thisRecord["tsn"],queryTESS(thisRecord["tsn"]))
 
     if '"result"=>"none"' in thisRecord["tessPairs"] and type(thisRecord["acceptedTSN"]) is str:
         # Query based on discovered TSN and package data
